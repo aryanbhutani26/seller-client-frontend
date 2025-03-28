125a26730c7363d28d0e1b29b039fd4ccada3ea8
aryanbhutani26 <159881202+aryanbhutani26@users.noreply.github.com>
2025-03-28 23:43:23 +0530

    Done

--- components/landing-page-components/Landing_Nav.tsx
+++ components/landing-page-components/Landing_Nav.tsx
@@ -18,15 +18,15 @@ const Landing_Nav: React.FC = () => {
     <footer>
       <nav className="navbar flex justify-between items-center px-7 py-5 container mx-auto">
        <Link href="/" className="flex items-center">
-          <Image className="w-14 lg:h-14 lg:w-18 lg:h-18" src="/ClothBuddyLogo.png" alt="ClothBuddy Logo" width={56} height={56} />
+          <Image className="w-14 lg:h-14 lg:w-18 lg:h-18 mr-5" src="/ClothBuddyLogo.png" alt="ClothBuddy Logo" width={56} height={56} />
           <h1 className="hidden text-lg font-semibold text-black sm:block lg:text-xl">ClothBuddy</h1>
         </Link>
 
         {/* Desktop Menu */}
         <div className="hidden md:flex space-x-6 items-center">
-          <Link href="/signup" className="text-lg font-normal hover:underline">
+          {/* <Link href="/signup" className="text-lg font-normal hover:underline">
             Sell With Us
-          </Link>
+          </Link> */}
           <Link href="/" className="text-lg font-normal hover:underline">
             Success Stories
           </Link>
@@ -34,7 +34,7 @@ const Landing_Nav: React.FC = () => {
             href="/signup"
             className="bg-black hover:bg-[#252525] text-white font-bold py-2 px-4 rounded-full"
           >
-            Signup/Login
+            Sell With Us
           </Link>
         </div>
 
